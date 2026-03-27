31c6c8807ec579d8997228cf9017696d630dd505
Esra Tetik <tetikesra48@gmail.com>
2026-03-27 11:54:34 +0300

    Add files via upload

---
+++ KarakterVeriTipleri.java
@@ -0,0 +1,14 @@
+public class KarakterVeriTipleri {
+    public static void main (String[] args){
+        char karakter = 117;
+        System.out.println(karakter);
+
+        boolean mantik = true;
+        System.out.println(mantik);
+
+        String str="Merhaba Esra.";
+        System.out.println(str);
+    
+    }
+
+}
