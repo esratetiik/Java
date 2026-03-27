d3a2d2d00bf0c491f2544d211364456f74c8c6b1
Esra Tetik <tetikesra48@gmail.com>
2026-03-27 11:40:52 +0300

    Add files via upload

---
+++ SayisalVeriTipleri.java
@@ -0,0 +1,16 @@
+public class Merhaba {
+    public static void main (String[] args){
+        byte b = 100;
+        System.out.println("byte = " + b);
+
+        short s=1000;
+        System.out.println("short = " + s);
+
+        int i=100000;
+        System.out.println("integer = " +i);
+
+        long l=100000000;
+        System.out.println("long = " +l);
+    }
+       
+}
