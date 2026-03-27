f62131a29fee96e3f703d58557810ce2e1095c5a
Esra Tetik <tetikesra48@gmail.com>
2026-03-27 11:16:49 +0300

    Add files via upload

---
+++ Merhaba.java
@@ -0,0 +1,15 @@
+public class Merhaba {
+
+    public static void main (String[] args){
+        System.out.print("Hello World!");
+        System.out.print("Hello");
+        System.out.println("Hello"); //bir sonrakini alt satırdan başlatır.
+        System.out.println("Java derslerine hoş geldiniz."
+            + "Java ile nesne yönelimli kodlama.");
+        System.out.println(10 + "15");
+        System.out.println(10+15);
+        System.out.print("Merhaba\n");
+        System.out.print("Dünya");
+
+    }
+}
