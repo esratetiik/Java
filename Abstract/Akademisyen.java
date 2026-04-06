17ba91bd7b3525208167cf022a2acdcb65b232b4
Esra Tetik <tetikesra48@gmail.com>
2026-04-06 17:12:00 +0300

    Add files via upload

---
+++ Abstract/Akademisyen.java
@@ -0,0 +1,20 @@
+
+public abstract class Akademisyen extends Calisan{
+    
+    String bolum, gorevler;
+    String ders;
+
+    public Akademisyen(String adSoyad, String eposta, String telefon, String bolum, String gorevler, String ders){
+        super(adSoyad, eposta, telefon);
+        this.bolum = bolum;
+        this.gorevler  = gorevler;
+        this.ders = ders;
+    }
+    
+    @Override
+    public void giris(){
+        System.out.println(this.adSoyad + " akademisyen girişi yaptı.");
+    }
+
+   
+}
